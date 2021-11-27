90cfee8381a08c83cc29db8ec32a11309d2d9e09
Roger Chang <d9352676@gmail.com>
2021-11-27 21:42:35 +0800

    Initial Commit

---
+++ BezierPathMeguminDemo/BezierPathMeguminDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BezierPathMeguminDemoApp.swift
+//  BezierPathMeguminDemo
+//
+//  Created by 張又壬 on 2021/11/27.
+//
+
+import SwiftUI
+
+@main
+struct BezierPathMeguminDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
